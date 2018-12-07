96037a8edb041a487af098858956be15db3f3d87
Ayman Salah <ayman171salah@gmail.com>
2018-12-07 19:53:41 +0200

    fixed a small bug

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
 function OpenNewTab(info , tab) {
-  // create new tab and go to googel translate
-  chrome.tabs.create({
-    url : "https://translate.google.com/#view=home&op=translate&sl=en&tl=ar&text=" + info.selectionText ; 
-  });
+  
 }
 
 
 chrome.contextMenus.create({
   title : "Arabic translate of: %s " ,
   contexts : ["selection"],
-  onclick : OpenNewTab
+  onclick : OpenNewTab ,
 });
