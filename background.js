d594e42b82cb4ec71411a3d332b4aabd32e970f2
Ayman Salah <ayman171salah@gmail.com>
2018-12-07 19:42:14 +0200

    Added the new-tab translate

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 function OpenNewTab(info , tab) {
-
+  // create new tab and go to googel translate
+  chrome.tabs.create({
+    url : "https://translate.google.com/#view=home&op=translate&sl=en&tl=ar&text=" + info.selectionText ; 
+  });
 }
 
 
